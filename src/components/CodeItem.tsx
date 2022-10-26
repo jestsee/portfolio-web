ced421f1ce921291c0121eaabca29386409d71c7
Jesica <13519011@std.stei.itb.ac.id>
2022-10-26 13:44:01 +0700

    add link to portfolio item

--- src/components/CodeItem.tsx
+++ src/components/CodeItem.tsx
@@ -19,10 +19,17 @@ export function CodeItem({
   githubUrl,
 }: Props) {
   return (
-    <span className="w-full rounded-2xl bg-slate-800 bg-cover bg-center px-8 py-10 shadow-xl shadow-black/10 hover:scale-105">
+    <div className="w-full rounded-2xl bg-slate-800 bg-cover bg-center px-8 py-10 shadow-xl shadow-black/10 hover:scale-105">
       <div className="flex justify-between">
         <FolderIcon className="h-10 w-10 text-teal-400" />
-        <div className="flex items-center gap-x-4">
+        <a
+          href={projectUrl || githubUrl}
+          aria-label="Project Link"
+          target="_blank"
+        >
+          <span className="absolute top-0 left-0 z-[1] h-full w-full"></span>
+        </a>
+        <div className="z-[2] flex items-center gap-x-4">
           {projectUrl && (
             <a href={projectUrl} aria-label="Project URL" target="_blank">
               <OpenIcon className="h-6 w-6 text-smoke hover:text-teal-400" />
@@ -44,6 +51,6 @@ export function CodeItem({
           })}
         </p>
       </div>
-    </span>
+    </div>
   )
 }
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -9,7 +9,7 @@ export function Portfolio() {
       <LargeText className="mb-4 text-4xl max-sm:text-2xl">
         What I am <span className="text-teal-400">working on</span>
       </LargeText>
-      <p className="text-center opacity-50">
+      <p className="text-center opacity-80">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium
         nemo blanditiis cum fugit doloribus tempora accusamus.
       </p>
