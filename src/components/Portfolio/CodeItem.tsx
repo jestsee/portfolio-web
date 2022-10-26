ec9bf3caafcbbbbb96ff9d9e54a4d09e617de377
Jesica <13519011@std.stei.itb.ac.id>
2022-10-26 18:00:10 +0700

    fix defect

--- src/components/Portfolio/CodeItem.tsx
+++ src/components/Portfolio/CodeItem.tsx
@@ -22,13 +22,6 @@ export function CodeItem({
     <div className="w-full rounded-2xl bg-slate-800 bg-cover bg-center px-8 py-10 shadow-xl shadow-black/10 hover:scale-105">
       <div className="flex justify-between">
         <FolderIcon className="h-10 w-10 text-teal-400" />
-        <a
-          href={projectUrl || githubUrl}
-          aria-label="Project Link"
-          target="_blank"
-        >
-          <span className="absolute top-0 left-0 z-[1] h-full w-full"></span>
-        </a>
         <div className="z-[2] flex items-center gap-x-4">
           {projectUrl && (
             <a href={projectUrl} aria-label="Project URL" target="_blank">
